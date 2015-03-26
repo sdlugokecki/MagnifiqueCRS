eff7af487aa8eaa9050a3fa0111fae990b189c9b
axelkossek <axelkossek@gmail.com>
2015-03-26 14:26:03 +0100

    super commit
    super

---
+++ MagnifiqueCRM/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MagnifiqueCRM.Startup))]
+namespace MagnifiqueCRM
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
